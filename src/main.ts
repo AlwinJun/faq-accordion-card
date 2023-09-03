0eb0d2e53283285eca6a55a1cf26a9bf0d635ee4
AJ <mahidlawonaljun@gmail.com>
2023-09-03 21:31:52 +0800

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,23 @@ import './styles/css/style.css';
 
 // console.log('test')
 
-document.querySelectorAll('.accordion__question').forEach((button) => {
-  button.addEventListener('click', () => {
-    const acoordionText = button.nextElementSibling.firstElementChild;
-    const buttonTag = button.firstElementChild;
-    buttonTag.classList.toggle('show');
-    buttonTag.firstElementChild.classList.toggle('show');
-    acoordionText.classList.toggle('show');
+const questions = document.querySelectorAll('.accordion__question') as NodeListOf<HTMLHeadingElement>;
 
-    if (acoordionText.classList.contains('show')) {
-      acoordionText.style.height = acoordionText.scrollHeight / 16 + 'rem';
-    } else {
-      acoordionText.style.height = '0rem';
-    }
-  });
+questions.forEach((accordionQuestion: HTMLHeadingElement) => {
+  accordionQuestion.addEventListener('click', handleClick);
 });
+
+function handleClick(event: Event) {
+  const accordionQuestion = event.currentTarget as HTMLHeadingElement;
+  const accordionContent = accordionQuestion.nextElementSibling?.firstElementChild as HTMLParagraphElement;
+  const buttonTag = accordionQuestion.firstElementChild as HTMLButtonElement;
+  const spanElement = buttonTag.firstElementChild as HTMLSpanElement;
+
+  buttonTag.classList.toggle('show');
+  spanElement.classList.toggle('show');
+  accordionContent.classList.toggle('show');
+
+  const scrollHeight: number = accordionContent.scrollHeight;
+  accordionContent.style.height = accordionContent.classList.contains('show') ? `${scrollHeight / 16}rem` : '0rem';
+}
 
